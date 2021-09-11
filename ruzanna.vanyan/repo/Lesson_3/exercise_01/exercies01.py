377c30d1e0c401484cbcafb41c976821c2b8be40
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-11 22:27:24 +0400

    Exercise_01 version1

---
+++ ruzanna.vanyan/repo/Lesson_3/exercise_01/exercies01.py
@@ -0,0 +1,18 @@
+print( "*" * 19 + "\n" + "*" * 19 +  "\n" + "*" * 19 + "\n" + "*" * 19)
+
+
+line_1 = "*" * 19
+line_2 = "*" * 19
+line_3 = "*" * 19
+line_4 = "*" * 19
+print(line_1 + "\n" + line_2 + "\n" + line_3 + "\n" + line_4 )
+
+
+line_1 = "*" * 19
+line_2 = "*" * 19
+line_3 = "*" * 19
+line_4 = "*" * 19
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
