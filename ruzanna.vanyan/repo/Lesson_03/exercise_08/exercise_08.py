fe5400f0b110671a5c3337e3af1ba32de9067fe2
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-12 00:18:55 +0400

    Exercise_09 version1

---
+++ ruzanna.vanyan/repo/Lesson_03/exercise_08/exercise_08.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+num_3 = int(input("Enter a number: "))
+total = num_1 + num_2 + num_3
+average = total / 3
+print(f"The numbers total is: {total}, and average is {average}" )
