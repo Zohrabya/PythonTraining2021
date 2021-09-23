d1149bbc95379ed712fb1e6d8165eebaedaabe78
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-23 23:24:51 +0400

    Exercise_9 Version2

--- ruzanna.vanyan/repo/Lesson_03/exercise_09/exercise_09.py
+++ ruzanna.vanyan/repo/Lesson_03/exercise_09/exercise_09.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-meal = int(input("Enter the price of your meal: "))
-percent = int(input("Enter the percent of tip that you want to leave: "))
-tip = int((meal * percent) / 100)
-bill = int(meal + tip)
-print(f"Your tip amount is {tip} and the total bill is {bill} ")
+meal = int(input("Enter the price of your meal."))
+percent = int(input("Enter the percent of tip that you want to leave."))
+tip = ((meal * percent) / 100)
+bill = (meal + tip)
+print("Your tip amount is", tip, "and the total bill is", bill)
