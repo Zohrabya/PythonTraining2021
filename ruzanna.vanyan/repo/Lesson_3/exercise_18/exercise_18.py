d8b5733c20e1c15af7c8f776f1f6c1cd852eadb5
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-23 23:03:37 +0400

    Exercise_18 Version2

--- ruzanna.vanyan/repo/Lesson_3/exercise_18/exercise_18.py
+++ ruzanna.vanyan/repo/Lesson_3/exercise_18/exercise_18.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 radius = int(input("Enter the radius of circle: "))
-Pi =  3.14159
+PI =  3.14159
 print("The circle's diameter is: ", round(radius * 2, 2))
-print("The circle's circumference is: ", round(radius * 2 * Pi, 2))
-print("The circle's area is: ", round(Pi * radius ** 2, 2))
+print("The circle's circumference is: ", round(radius * 2 * PI, 2))
+print("The circle's area is: ", round(PI * radius ** 2, 2))
