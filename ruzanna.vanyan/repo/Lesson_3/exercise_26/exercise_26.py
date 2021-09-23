2ddf1fab1ac2b52d4aa4223b6ac454c2a1e2144e
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-23 21:32:51 +0400

    Exercise_26 Version2

--- ruzanna.vanyan/repo/Lesson_3/exercise_26/exercise_26.py
+++ ruzanna.vanyan/repo/Lesson_3/exercise_26/exercise_26.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-#option1
-name = ("ruzanna vanyan")
-print(name.title())
-
-#option2
 name = str(input("Enter your passport name: " ))
 surname = str(input("Enter your surname: "))
 print(name.capitalize(), surname.capitalize()) 
