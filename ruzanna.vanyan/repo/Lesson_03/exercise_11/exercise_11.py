788f1e1105c8feb64bb88dc8a1f9dbd07f9aceb1
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-24 19:20:25 +0400

    Exercise_11 Version2

--- ruzanna.vanyan/repo/Lesson_03/exercise_11/exercise_11.py
+++ ruzanna.vanyan/repo/Lesson_03/exercise_11/exercise_11.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-length = float(input("Enter the length of the field in feet: "))
-width = float(input("Enter the width of the field in feet: "))
-acre = (length * width) / 43560
-print("The area of the field in acre is: ", round(acre, 2))
+length = float(input("Enter the length of the field in feet:"))
+width = float(input("Enter the width of the field in feet:"))
+feet = (length * width) 
+acre = 43560 #square feet
+print("The area of the field in acre is:", round(feet / acre, 2))
