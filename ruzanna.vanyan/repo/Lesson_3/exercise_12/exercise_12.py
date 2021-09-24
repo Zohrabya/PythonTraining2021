3d97b3ac34f5ac4a0ef326711ff87e2ecbce7bd5
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-24 19:25:49 +0400

    Exercise_12 version2

--- ruzanna.vanyan/repo/Lesson_3/exercise_12/exercise_12.py
+++ ruzanna.vanyan/repo/Lesson_3/exercise_12/exercise_12.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
-cont_1 = float(input("Enter tha number of containers which are holding one liter or less: "))
-cont_2 = float(input("Enter the number of containers which are holding more than one liter: "))
-print("Your refund is", round(cont_1 * 0.10, 2) + round(cont_2 * 0.25, 2), "$")
+num1 = float(input("Enter tha number of containers which are holding one liter or less: "))
+num2 = float(input("Enter the number of containers which are holding more than one liter: "))
+dep1 = 0.10
+dep2 = 0.25
+print("Your refund is", round(num1 * dep1, 2) + round(num2 * dep2, 2), "$")
