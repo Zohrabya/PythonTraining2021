9b3464fbb4af355160514ff82de6a9f427b09ce1
Ruzanna Vanyan <ruzikvanyan@gmail.com>
2021-09-24 19:28:48 +0400

    Exercise_13 Version2

--- ruzanna.vanyan/repo/Lesson_3/exercise_13/exercise_13.py
+++ ruzanna.vanyan/repo/Lesson_3/exercise_13/exercise_13.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 cost = int(input("Enter the cost of your meal: "))
-tax = int(cost * 10 / 100)
-tip = int(cost * 18 / 100)
-total = int(cost + tax + tip)
+tax = cost * 10 / 100
+tip = cost * 18 / 100
+total = cost + tax + tip
 print("Your bill's tax amount is", round(tax, 2), "\n",  "The tip amount is", round(tip, 2),"\n", "The total price of the meal is", round(total, 2))
